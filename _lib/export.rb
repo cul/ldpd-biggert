8d75994805b61856c534f0c13bb067f2e9d6f8cd
mar-ii <marii@riseup.net>
2018-02-20 01:08:05 -0500

    fetch + add link to thumbnail img

--- _lib/export.rb
+++ _lib/export.rb
@@ -1,14 +1,18 @@
 $LOAD_PATH.unshift('_lib')
 require 'json'
 require 'hyacinth_export'
+require 'thumbnail'
+require 'tqdm'
 
 docs = Hyacinth::Export.to_json('_data/biggert-items.csv')
-docs.each do |doc|
-  doc['pid'] = doc['_identifiers'] #.detect {|x| x =~ /ave_biggert_\d+/}
+docs.tqdm.each do |doc|
+  doc['pid'] = doc['_identifiers']
   doc['_identifiers'].delete(doc['pid'])
   ids = doc.delete('_identifiers')
   doc['identifiers'] = ids unless ids.empty?
   doc.delete('fedora_pid')
   doc['doi'] = doc['doi'].tr('doi:','')
+  doc['thumbnail'] = thumbnail(doc['doi'],rand(1...4))
+  doc['coordinates'].each{ |c| c.tr!('°','') }
 end
 $stdout.puts JSON.pretty_generate(docs)
