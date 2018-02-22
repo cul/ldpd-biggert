f136fcbb7bd648a81e135d962d78290400063d4c
Benjamin Armintor <armintor@gmail.com>
2018-02-22 18:07:00 -0500

    brute force year ranges ('1880-1900')

--- js/lunr-ui.js
+++ js/lunr-ui.js
@@ -1,3 +1,42 @@
+elasticlunr.Index.prototype.rangeSearch = function (queryTokens, fieldName) {
+// range tokens should be 'expanded' to appropriate values
+  queryTokens = queryTokens.map(function(x){ return parseInt(x); });
+  queryTokens.sort;
+  newTokens = new Array(queryTokens[1] - queryTokens[0] + 1).fill(null)
+  .map(function(val,ix) { return (ix + queryTokens[0]).toString(); });
+  queryTokens = newTokens;
+  var userConfig = {fields: {}, boost: 0};
+  userConfig.fields[fieldName] = {boost: 1, bool: "OR", expand: false};
+  var configStr = JSON.stringify(userConfig);
+  var config = new elasticlunr.Configuration(configStr, this.getFields()).get();
+  var queryResults = {};
+
+  for (var field in config) {
+    var fieldSearchResults = this.fieldSearch(queryTokens, field, config);
+    var fieldBoost = config[field].boost;
+
+    for (var docRef in fieldSearchResults) {
+      fieldSearchResults[docRef] = fieldSearchResults[docRef] * fieldBoost;
+    }
+
+    for (var docRef in fieldSearchResults) {
+      if (docRef in queryResults) {
+        queryResults[docRef] += fieldSearchResults[docRef];
+      } else {
+        queryResults[docRef] = fieldSearchResults[docRef];
+      }
+    }
+  }
+
+  var results = [];
+  for (var docRef in queryResults) {
+    results.push({ref: docRef, score: queryResults[docRef]});
+  }
+
+  results.sort(function (a, b) { return b.score - a.score; });
+  return results;
+};
+
 $(document).ready(function() {
   $('input#search-text').on('keyup', function () {
     var results_div = $('#results');
@@ -9,9 +48,13 @@ $(document).ready(function() {
       params.fields = {};
       params.fields[fields[0]] =  {boost: 2};
       params.boost = 0;
-      console.log(params);
     }
-    var results = index.search(query, params);
+    var results = null;
+    if (query.match(/\d+[-]\d+/)) {
+      results = index.rangeSearch(query.split(/[-]/),'date_other');
+    } else {
+      results = index.search(query, params);
+    }
     
     results_div.empty();
 
@@ -20,9 +63,11 @@ $(document).ready(function() {
       var ref     = results[r].ref;
       var item    = store[ref];
       var link    = item.link;
-      console.log(link);
       var pid    = link.split('\/')[2];
-      var title     = item.title  || '';
+      var title     = (item.title  || '');
+      if (item.date_other) {
+        title = title + ' (' + item.date_other + ')';
+      }
       var result  = '<div class="result"><b>' + pid + ':<br><a href="' + link + '">' + title + '</a></b></div>';
       results_div.append(result);
     }
