6b7630dfd2e0ae7127e07ebf77055b857398ca7b
Fred Duby <fcd1@columbia.edu>
2018-05-08 11:15:55 -0400

    Sampe code only: Execute jekyll build and wax push -- NOTE: destination branch won't work

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,3 +13,17 @@ set :deploy_to, "/opt/passenger/ldpd/dcv_dev/subsites/#{fetch(:application)}"
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+before 'deploy:starting', :foobar do
+  run_locally do
+    puts " Hi Fred, about to build"
+    # execute 'bundle exec jekyll build --verbose'
+    execute 'bundle exec jekyll build'
+    puts " Hi Fred, finished building"
+    puts " Hi Fred, about to wax push"
+    # execute 'bundle exec jekyll build --verbose'
+    execute 'bundle exec rake wax:push:static'
+    puts " Hi Fred, finished wax pushing"
+  end
+end
+
